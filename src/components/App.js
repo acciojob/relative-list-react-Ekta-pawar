e184454caba80721e2eacf9c2ad088eebea474c3
Ekta <ektapawar0001@gmail.com>
2025-08-08 17:33:38 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               
+               
     </div>
   )
 }
