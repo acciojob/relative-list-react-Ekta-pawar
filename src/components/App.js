3010565a61809237fb1ad22c2f32fbc5fd3ebe82
Ekta <ektapawar0001@gmail.com>
2025-08-31 11:36:13 +0530

    removed

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ const App = () => {
 
   return (
     <div id="main">
-      <h1>This is heading</h1>
         <ol key='relativeList'>
           {relatives.map((name,index)=>{
         <li key={`relativeListItem${index + 1}`}>
