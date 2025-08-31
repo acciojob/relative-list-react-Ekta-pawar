f796997e0c05bcadf57ee02697a1d9b24d2af246
Ekta <ektapawar0001@gmail.com>
2025-08-31 11:16:24 +0530

    my commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const relatives = ["Ramesh", "Suresh", "Geeta", "Meena"];
 
   return (
-    <div>
+    <div id="main">
         <ol id='relativeList'>
           {relatives.map((name,index)=>{
         <li key={`relativeListItem${index + 1}`}>
